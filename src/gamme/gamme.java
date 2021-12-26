eac17ba0b35d2d7cdcbc3d99d3ed0cf05f820aae
goguma9071 <gogumagogumayt@gmail.com>
2021-12-26 17:19:39 +0900

    0.1.1

--- src/gamme/gamme.java
+++ src/gamme/gamme.java
@@ -3,7 +3,14 @@ package gamme;
 import javax.swing.*;
 
 public class gamme extends JFrame {
+    public gamme(){
+        setTitle ("gamme");
+        setVisible(true);
+        setSize(500 , 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     public static void main(String[] args) {
-        setTitle("gamme");
+        new gamme();
     }
 }
