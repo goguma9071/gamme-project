722408ec8a74e3e5509829f2d452fef11c0b0773
goguma9071 <gogumagogumayt@gmail.com>
2021-12-29 15:39:37 +0900

    0.1.3

--- src/gamme/gamme.java
+++ src/gamme/gamme.java
@@ -2,15 +2,37 @@ package gamme;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class gamme extends JFrame {
     private Image backgroundImage = new ImageIcon("src/image/background.jpeg").getImage();
+    private boolean up, right , down , left;
     public gamme(){
         setTitle ("gamme");
         setVisible(true);
         setSize(500 , 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_UP:
+                        up = true;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        down = true;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        left = true;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        right = true;
+                        break;
+                }
+            }
+        });
     }
     public void paint(Graphics g) {
         g.drawImage(backgroundImage, 0, 0, null);
